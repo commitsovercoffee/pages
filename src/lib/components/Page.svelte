bad8287917d2d9fc6667817169cb4beb7520941c
commitsovercoffee <commitsovercoffee@gmail.com>
2023-11-28 00:04:17 +0530

    adds scroll snapping

--- src/lib/components/Page.svelte
+++ src/lib/components/Page.svelte
@@ -1,5 +1,5 @@
 <div
-	class="h-[80vh] pb-48 flex flex-col items-center justify-center text-4xl lg:text-6xl 2xl:text-8xl lg:text-center"
+	class="h-[100vh] flex flex-col items-center justify-center text-4xl lg:text-6xl 2xl:text-8xl lg:text-center snap-start"
 >
 	<section class="m-8 p-4 flex flex-col items-start">
 		<slot />
