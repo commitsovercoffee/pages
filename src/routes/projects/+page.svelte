c23d4f0a81095419dd2432d243c40f9940bbdbf0
commitsovercoffee <commitsovercoffee@gmail.com>
2023-11-29 19:49:05 +0530

    fixes project cards

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,16 +2,16 @@
 	import * as content from './content';
 
 	const tagStyle =
-		'no-underline px-2 mx-2 rounded-xl bg-stone-400  hover:bg-stone-900 hover:text-orange-50 transition-colors duration-200 ease-in ';
+		'no-underline px-2 mx-2 rounded-xl bg-stone-400 hover:bg-stone-900 hover:text-orange-50 transition-colors duration-200 ease-in ';
 </script>
 
 <section class="prose mx-auto">
 	<p class="m-4 text-xl 2xl:text-2xl">Some how I always find time to tinker. Check 'em out!</p>
 
-	<div class="flex flex-wrap">
+	<div class="grid grid-cols-1 sm:grid-cols-2">
 		{#each content.projects as project}
 			<div
-				class="m-4 p-4 border border-stone-400 rounded-xl flex flex-col grow justify-betwen hover:-translate-y-3 hover:shadow-lg hover:shadow-stone-400 transition-all duration-200 ease-in-out"
+				class=" m-4 p-2 border border-stone-400 rounded-xl flex flex-col grow justify-betwen hover:-translate-y-3 hover:shadow-lg hover:shadow-stone-400 transition-all duration-200 ease-in-out"
 			>
 				<div>
 					<p class="text-stone-900 font-semibold">
